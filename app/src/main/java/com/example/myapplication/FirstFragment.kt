1230da4be4d8d69fed31b7827672ce4efa52b8fd
Jose Jiménez <josexisco1993@gmail.com>
2023-09-22 13:16:04 +0200

    commit ok

---
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnTercerFragment.setOnClickListener{
+            findNavController().navigate(R.id.action_firstFragment_to_thirdFragment)
+        }
+        binding.btnCuartoFragment.setOnClickListener{
+            findNavController().navigate(R.id.action_firstFragment_to_fourthFragment)
+        }
+    }
+
+}
