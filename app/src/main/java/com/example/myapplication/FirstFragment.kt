211070ede5492634433217f626bf655730c1af27
JoseJimenezGonzalez <josexisco1993@gmail.com>
2023-09-22 18:43:41 +0200

    commit calcu

--- app/src/main/java/com/example/myapplication/FirstFragment.kt
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -14,6 +14,11 @@ class FirstFragment : Fragment() {
     private var _binding: FragmentFirstBinding? = null
     private val binding get() = _binding!!
 
+    //onCreateView()
+    //El sistema lo llama cuando el fragmento debe diseñar su interfaz de usuario por primera vez.
+    // A fin de diseñar una IU para tu fragmento, debes mostrar un View desde este método, que será
+    // la raíz del diseño de tu fragmento. Puedes mostrar un valor nulo si el fragmento no proporciona una IU.
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,6 +26,9 @@ class FirstFragment : Fragment() {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
     }
+    //onViewCreated()
+    //Called immediately after #onCreateView(LayoutInflater, ViewGroup, Bundle) has returned, but
+    // before any saved state has been restored in to the view.
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
