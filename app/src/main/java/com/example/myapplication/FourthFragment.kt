2c1c25a4396e56aefe25ab6e0264363bd139aa1e
Jose Jiménez <josexisco1993@gmail.com>
2023-09-22 13:11:09 +0200

    commit ok

---
+++ app/src/main/java/com/example/myapplication/FourthFragment.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.FragmentFourthBinding
+import com.example.myapplication.databinding.FragmentThirdBinding
+
+class FourthFragment : Fragment() {
+
+    private var _binding: FragmentFourthBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFourthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnCuartoToPrimerFragment.setOnClickListener{
+            findNavController().popBackStack()
+        }
+    }
+}
