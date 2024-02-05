167bd7cba873fff3ae300df9d21be297fb73640d
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-02-05 13:02:42 +0100

    commit ok

--- app/src/main/java/com/example/myapplication/SecondFragment.kt
+++ app/src/main/java/com/example/myapplication/SecondFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentSecondBinding
+import com.google.android.material.navigation.NavigationView
 
 class SecondFragment : Fragment() {
     private var _binding:FragmentSecondBinding? = null
@@ -17,6 +22,52 @@ class SecondFragment : Fragment() {
     ): View {
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Codigo
+
+        val drawerLayout: DrawerLayout = binding.secondFragment
+        val navView: NavigationView = binding.navView
+
+        val toggle = ActionBarDrawerToggle(
+            requireActivity(),
+            drawerLayout,
+            requireActivity().findViewById(R.id.toolbar), // Cambia esta línea
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val navController = findNavController()
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_add -> {
+
+                    navController.navigate(R.id.otherFragment)
+                    true
+                }
+                R.id.nav_see -> {
+                    val fragmentB = Other2Fragment()
+                    val transaction = childFragmentManager.beginTransaction()
+                    childFragmentManager.popBackStackImmediate() // Limpiar la pila de retroceso
+                    transaction.replace(R.id.secondFragment, fragmentB)
+                    transaction.addToBackStack(null)
+                    transaction.commit()
+                    true
+                }
+                R.id.nav_edit -> {
+                    // Lógica para la opción Editar
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 
 }
--- app/src/main/java/com/example/myapplication/ThirdFragment.kt
+++ app/src/main/java/com/example/myapplication/ThirdFragment.kt
@@ -3,8 +3,11 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.MenuRes
+import androidx.appcompat.widget.PopupMenu
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentFirstBinding
 import com.example.myapplication.databinding.FragmentThirdBinding
@@ -26,5 +29,45 @@ class ThirdFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         //Codigo
+
+        binding.ivPopupMenu.setOnClickListener {v: View ->
+            showMenu(v, R.menu.popup_menu)
+        }
     }
+
+    private fun showMenu(v: View, @MenuRes menuRes: Int) {
+        val popup = PopupMenu(requireContext(), v)
+        popup.menuInflater.inflate(menuRes, popup.menu)
+
+        popup.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_add -> {
+
+                    val navController = findNavController()
+
+                    val transaction = requireActivity().supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.nav_host_fragment, Popup1Fragment())
+                    transaction.addToBackStack(null)
+                    transaction.commit()
+
+
+                    true
+                }
+                R.id.nav_edit -> {
+                    //navigateToDestination(R.id.)
+                    true
+                }
+                // Agrega más casos según tus opciones de menú
+                else -> false
+            }
+        }
+
+        popup.setOnDismissListener {
+
+        }
+
+        // Show the popup menu.
+        popup.show()
+    }
+
 }
